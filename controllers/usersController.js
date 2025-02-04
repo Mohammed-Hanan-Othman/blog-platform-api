9398ae2a1dbf8fcc70fac60f53810678b15fe48a
mohammed-hanan-othman <mohananothman@gmail.com>
2025-02-04 03:54:08 +0000

    Defined base structure for users controller and router

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,1 +1,19 @@
 // This file would contain code implemented by users router
+const getAllUsers = async (req, res) => {
+    
+};
+const getSingleUser = async (req, res) => {
+    
+};
+const deleteAllUsers = async (req, res) => {
+    
+};
+const updateMyInfo = async (params) => {
+    
+};
+module.exports = {
+    getAllUsers,
+    getSingleUser,
+    deleteAllUsers,
+    updateMyInfo
+};
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -2,4 +2,18 @@
 const { Router } = require("express");
 const usersRouter = Router();
 
+// Handles /api/users
+usersRouter.get("/", (req, res) => {
+    res.status(200).json({message:"See all the users here"});
+});
+usersRouter.put("/me", (req, res) => {
+    res.status(200).json({message:"Update user's personal information"});
+});
+usersRouter.get("/:id", (req, res) => {
+    res.status(200).json({message:"Info on specific user: Admin only"});
+});
+usersRouter.delete("/:id", (req, res) =>{
+    res.status(200).json({message:"Delete a specific user"});
+});
+
 module.exports = { usersRouter };
