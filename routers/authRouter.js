0b1e34164f464d9f281143ba08010e3fab5884c8
mohammed-hanan-othman <mohananothman@gmail.com>
2025-02-15 17:13:09 +0000

    Added routes for forgot password functionality

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -24,5 +24,15 @@ authRouter.post(
     handleValidationErrors,
     postLogin
 );
+// password reset
+authRouter.post("/forgot-password",(req, res)=>{
+    // Send reset code to user's email
+});
+authRouter.post("/verify-reset-code",(req, res)=>{
+    // Verify reset code
+});
+authRouter.post("/reset-password",(req, res)=>{
+    // Reset the password
+});
 
 module.exports = { authRouter };
